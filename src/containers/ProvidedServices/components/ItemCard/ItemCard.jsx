823ed173bed846649df42cd06d7ab058bbe1b314
AndrRomanenko <andy_snews@mail.ru>
2018-11-19 14:26:10 +0200

    styling

--- src/containers/ProvidedServices/components/ItemCard/ItemCard.jsx
+++ src/containers/ProvidedServices/components/ItemCard/ItemCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
+import { string } from 'prop-types';
 import styles from './ItemCard.scss';
-
 
 const ItemCard = ({ image, title, description }) => (
   <div className={styles.itemCard}>
@@ -10,5 +10,17 @@ const ItemCard = ({ image, title, description }) => (
     <div className={styles.description}>{description}</div>
   </div>
 );
+
+ItemCard.propTypes = {
+  image: string,
+  title: string,
+  description: string,
+};
+
+ItemCard.defaultProps = {
+  image: 'Empty',
+  title: 'Empty',
+  description: 'sample text',
+};
 
 export default ItemCard;
