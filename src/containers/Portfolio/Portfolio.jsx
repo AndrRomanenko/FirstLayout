fa66a8bde1f9b1f635647d31ad048db6ea414e9f
AndrRomanenko <andy_snews@mail.ru>
2018-11-20 15:56:33 +0200

    added Menu component to Portfolio

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Title from 'Shared/Title';
 import Dash from 'Shared/Dash';
 import Description from 'Shared/Description';
+import Menu from './components/Menu';
 import styles from './Portfolio.scss';
 
 const Portfolio = () => (
@@ -14,6 +15,7 @@ const Portfolio = () => (
       text="Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet
        consectetur, adipisci velit, sed quia non numquam"
     />
+    <Menu buttons={['All', 'Web', 'Apps', 'Icons']} />
   </div>
 );
 
---
+++ src/containers/Portfolio/components/Menu/Menu.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+import Button from 'Shared/Button';
+import styles from './Menu.scss';
+
+const Menu = ({ buttons }) => (
+  <div className={styles.container}>
+    {
+      buttons.map(button => (
+        <Button
+          btnType="button"
+          className={styles.menuButton}
+          label={button}
+        />
+      ))
+    }
+  </div>
+);
+export default Menu;
