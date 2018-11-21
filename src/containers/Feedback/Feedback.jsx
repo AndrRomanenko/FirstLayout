bc37e122bdce35296ac077f9da98e4d983fd37de
AndrRomanenko <andy_snews@mail.ru>
2018-11-21 17:57:33 +0200

    FeedBack container added

---
+++ src/containers/Feedback/Feedback.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Title from 'Shared/Title';
+import Dash from 'Shared/Dash';
+import Description from 'Shared/Description';
+
+import styles from './Feedback.scss';
+
+const Feedback = () => (
+  <div className={styles.feedback}>
+    <Title text="GET IN TOUCH" />
+    <Dash />
+    <Description text="1600 Pennsylvania Ave NW, Washington, DC 20500, United States of America. Tel: (202) 456-1111" />
+  </div>
+);
+
+export default Feedback;
