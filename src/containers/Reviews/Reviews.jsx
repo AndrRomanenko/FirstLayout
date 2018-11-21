6e5e8c19949aa86aba87deaec707dfe2e6c9f052
AndrRomanenko <andy_snews@mail.ru>
2018-11-21 13:23:08 +0200

    component ReviewCard added to Review component

--- src/containers/Reviews/Reviews.jsx
+++ src/containers/Reviews/Reviews.jsx
@@ -2,15 +2,48 @@ import React from 'react';
 import Title from 'Shared/Title';
 import Dash from 'Shared/Dash';
 import Description from 'Shared/Description';
+import ReviewCard from './components/ReviewCard';
 
 import styles from './Reviews.scss';
 
-const Reviews = () => (
-  <div className={styles.container}>
-    <Title text="WHAT POEPLE SAY ABOUT US" />
-    <Dash />
-    <Description text="Our clients love us!" />
-  </div>
-);
+const Reviews = () => {
+  const reviews = [
+    {
+      review: '“Nullam dapibus blandit orci, viverra gravida dui lobortis eget. Maecenas fringilla urna eu nisl scelerisque.”',
+      name: 'Chanel Iman',
+      position: 'CEO of Pinterest',
+    },
+    {
+      review: '“Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta.”',
+      name: 'ADRIANA LIMA',
+      position: 'Founder of Instagram',
+    },
+    {
+      review: '“Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta.”',
+      name: 'ANNE HATHAWAY',
+      position: 'Lead Designer at Behance',
+    },
+    {
+      review: '“Phasellus non purus vel arcu tempor commodo. Fusce semper, purus vel luctus molestie, risus sem cursus neque.”',
+      name: 'EMMA STONE',
+      position: 'Co-Founder of Shazam',
+    },
+  ];
+
+  return (
+    <div className={styles.container}>
+      <Title text="WHAT POEPLE SAY ABOUT US" />
+      <Dash />
+      <Description text="Our clients love us!" />
+      <div className={styles.contentContainer}>
+        {
+         reviews.map(rewiew => (
+           <ReviewCard {...rewiew} />
+         ))
+       }
+      </div>
+    </div>
+  );
+};
 
 export default Reviews;
---
+++ src/containers/Reviews/components/ReviewCard/ReviewCard.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import { string } from 'prop-types';
+import defaultPhoto from './assets/photo.svg';
+
+import styles from './ReviewCard.scss';
+
+const ReviewCard = ({
+  photo, name, position, review,
+}) => (
+  <div className={styles.card}>
+    <div className={styles.photoContainer}>
+      <img src={photo} alt={photo} />
+    </div>
+    <div className={styles.contentContainer}>
+      <div className={styles.review}>{review}</div>
+      <h2 className={styles.name}>{name}</h2>
+      <div className={styles.position}>{position}</div>
+    </div>
+  </div>
+);
+
+ReviewCard.propTypes = {
+  photo: string,
+  name: string,
+  position: string,
+  review: string,
+};
+
+ReviewCard.defaultProps = {
+  photo: defaultPhoto,
+  name: 'Igor Verkhoglyad',
+  position: 'Top JS developer',
+  review: 'Its the best layout ive ever seen. Just MF perfect!',
+};
+
+export default ReviewCard;
